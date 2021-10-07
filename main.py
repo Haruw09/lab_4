54e69c5204f6ab9ceef5f43d1b8b7efd1d327e49
NailKhas <nail230103@gmail.com>
2021-10-07 20:15:59 +0300

    Рефакторинг собак

--- main.py
+++ main.py
@@ -23,17 +23,15 @@ screen.fill(forestgreen)
 def fence(x, y, width, height):
     """
 
-    :param x:
-    :param y:
-    :param width:
-    :param height:
+    :param x: абцисса верхнего левого угла забора
+    :param y: ордината верхнего левого угла забора
+    :param width: длина забора
+    :param height: высота забора
     :return:
     """
     rect(screen, peru, (x, y, width, height))
     for j in range(width // 20):
         rect(screen, black, (x + j * 20, y, 20, height), 1)
-
-
 
 
 fence(0, 0, 300, 160)
@@ -53,53 +51,57 @@ polygon(screen, black, [(80, 160), (100, 150), (100, 206), (80, 213)])
 
 
 def chain(x, y, size, length):
+    """
+
+    :param x: абцисса первого звена
+    :param y: ордината первого звена
+    :param size: размер звеньев
+    :param length: количество звеньев
+    :return:
+    """
     X = x
     Y = y
     for i in range(length):
         fate = random.randint(0, 1)
-        ellipse(screen, black, [X, Y, (3 - (-1) ** fate)*size * 3, (3 - (-1) ** (fate + 1))*size * 3], 2 * size)
+        ellipse(screen, black, [X, Y, (3 - (-1) ** fate) * size * 3, (3 - (-1) ** (fate + 1)) * size * 3], 2 * size)
         X = X + (2 - (-1) ** fate) * 2 * size
         Y = Y + (2 - (-1) ** (fate + 1)) * 2 * size
-
-
-'''
-Функция рисует цепь.
-x, y - координаты первого звена
-size - размер звена 
-length - количество звеньев
-'''
 
 
 chain(80, 213, 1, 20)
 
 
 def dog(x, y, size):
-    ellipse(screen, sienna, [x, y, 10*math.sqrt(size**2), 30*math.sqrt(size**2)])
-    ellipse(screen, sienna, [x + 5*size, y + 25*size, 15*math.sqrt(size**2), 10*math.sqrt(size**2)])
-    ellipse(screen, sienna, [x + 60*size, y + 10*size, 10*math.sqrt(size**2), 30*math.sqrt(size**2)])
-    ellipse(screen, sienna, [x + 65*size, y + 35*size, 15*math.sqrt(size**2), 10*math.sqrt(size**2)])
-    ellipse(screen, sienna, [x + 40*size, y + 20*size, 10*math.sqrt(size**2), 30*math.sqrt(size**2)])
-    ellipse(screen, sienna, [x + 45*size, y + 45*size, 15*math.sqrt(size**2), 10*math.sqrt(size**2)])
-    ellipse(screen, sienna, [x - 20*size, y + 10*size, 10*math.sqrt(size**2), 30*math.sqrt(size**2)])
-    ellipse(screen, sienna, [x - 15*size, y + 35*size, 15*math.sqrt(size**2), 10*math.sqrt(size**2)])
-    ellipse(screen, sienna, [x - 15*size, y - 10*size, 80*math.sqrt(size**2), 40*math.sqrt(size**2)])
-    rect(screen, sienna, (x + 45*size, y - 30*size, 30*math.sqrt(size**2), 30*math.sqrt(size**2)))
-    ellipse(screen, saddlebrown, [x + 45*size, y - 30*size, 10*math.sqrt(size**2), 20*math.sqrt(size**2)])
-    ellipse(screen, saddlebrown, [x + 70*size, y - 30*size, 10*math.sqrt(size**2), 20*math.sqrt(size**2)])
-    ellipse(screen, white, [x + 50*size, y - 25*size, 7*math.sqrt(size**2), 5*math.sqrt(size**2)])
-    ellipse(screen, white, [x + 65*size, y - 25*size, 7*math.sqrt(size**2), 5*math.sqrt(size**2)])
-    circle(screen, black, (x + 53.5*size, y - 22*size), 2.5*math.sqrt(size**2))
-    circle(screen, black, (x + 68.5*size, y - 22*size), 2.5*math.sqrt(size**2))
-    rect(screen, black, (x + 55*size, y - 10*size, 10*math.sqrt(size**2), 2*math.sqrt(size**2)))
-    polygon(screen, white, [(x + 55*size, y - 10*size), (x + 56*size, y - 12*size), (x + 57*size, y - 10*size)])
-    polygon(screen, white, [(x + 63*size, y - 10*size), (x + 64*size, y - 12*size), (x + 65*size, y - 10*size)])
+    """
+
+    :param x: абцисса правого уха собаки
+    :param y: ордината правого уха собаки
+    :param size: размер
+    :return:
+    """
+    ellipse(screen, sienna, [x, y, 10 * math.sqrt(size ** 2), 30 * math.sqrt(size ** 2)])
+    ellipse(screen, sienna, [x + 5 * size, y + 25 * size, 15 * math.sqrt(size ** 2), 10 * math.sqrt(size ** 2)])
+    ellipse(screen, sienna, [x + 60 * size, y + 10 * size, 10 * math.sqrt(size ** 2), 30 * math.sqrt(size ** 2)])
+    ellipse(screen, sienna, [x + 65 * size, y + 35 * size, 15 * math.sqrt(size ** 2), 10 * math.sqrt(size ** 2)])
+    ellipse(screen, sienna, [x + 40 * size, y + 20 * size, 10 * math.sqrt(size ** 2), 30 * math.sqrt(size ** 2)])
+    ellipse(screen, sienna, [x + 45 * size, y + 45 * size, 15 * math.sqrt(size ** 2), 10 * math.sqrt(size ** 2)])
+    ellipse(screen, sienna, [x - 20 * size, y + 10 * size, 10 * math.sqrt(size ** 2), 30 * math.sqrt(size ** 2)])
+    ellipse(screen, sienna, [x - 15 * size, y + 35 * size, 15 * math.sqrt(size ** 2), 10 * math.sqrt(size ** 2)])
+    ellipse(screen, sienna, [x - 15 * size, y - 10 * size, 80 * math.sqrt(size ** 2), 40 * math.sqrt(size ** 2)])
+    rect(screen, sienna, (x + 45 * size, y - 30 * size, 30 * math.sqrt(size ** 2), 30 * math.sqrt(size ** 2)))
+    ellipse(screen, saddlebrown, [x + 45 * size, y - 30 * size, 10 * math.sqrt(size ** 2), 20 * math.sqrt(size ** 2)])
+    ellipse(screen, saddlebrown, [x + 70 * size, y - 30 * size, 10 * math.sqrt(size ** 2), 20 * math.sqrt(size ** 2)])
+    ellipse(screen, white, [x + 50 * size, y - 25 * size, 7 * math.sqrt(size ** 2), 5 * math.sqrt(size ** 2)])
+    ellipse(screen, white, [x + 65 * size, y - 25 * size, 7 * math.sqrt(size ** 2), 5 * math.sqrt(size ** 2)])
+    circle(screen, black, (x + 53.5 * size, y - 22 * size), 2.5 * math.sqrt(size ** 2))
+    circle(screen, black, (x + 68.5 * size, y - 22 * size), 2.5 * math.sqrt(size ** 2))
+    rect(screen, black, (x + 55 * size, y - 10 * size, 10 * math.sqrt(size ** 2), 2 * math.sqrt(size ** 2)))
+    polygon(screen, white,
+            [(x + 55 * size, y - 10 * size), (x + 56 * size, y - 12 * size), (x + 57 * size, y - 10 * size)])
+    polygon(screen, white,
+            [(x + 63 * size, y - 10 * size), (x + 64 * size, y - 12 * size), (x + 65 * size, y - 10 * size)])
 
 
-'''
-Функция рисует собаку.
-x, y - координаты правого уха собаки
-size - размер
-'''
 dog(145, 160, 1)
 dog(200, 230, 1.3)
 dog(-80, 270, 2)
